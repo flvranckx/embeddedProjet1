83d76ff32b01274f50ea385721e65acc2136a5b1
flvranckx <florian.vranckx@student.uclouvain.be>
2020-05-29 18:02:20 +0200

    m

--- root.c
+++ root.c
@@ -1,183 +1,4 @@
 
-/*
-#include "contiki.h"
-#include "random.h"
-#include "net/rime/rime.h"
-#include <stdbool.h> 
-#include <stdlib.h>
-#include <stdio.h> 
-
-bool treeBuilded = false; 
-
-static struct etimer et;
-bool registered =false;
-struct broadcastMessage{
-  bool treebuilder; 
-  bool reset; 
-  unsigned int hop;
-};
-struct unicastMessage{
-  linkaddr_t dest_addr;
-  linkaddr_t from_addr;
-  bool data;
-  bool reg;
-};
-struct rootingTable{
-  struct rootingTable * next;
-  linkaddr_t addr; 
-  linkaddr_t nextHop;
-};
-struct rootingTable * myRootingTable = NULL;
-PROCESS(root, "discovery process");
-AUTOSTART_PROCESSES(&root);
-
-static struct broadcast_conn broadcast;
-static struct unicast_conn uc;
-void addToRootingTable(linkaddr_t addr){
-  struct rootingTable * r= (struct rootingTable*)  malloc(sizeof(struct rootingTable));
-  r-> addr = addr;
-  struct rootingTable * current = myRootingTable; 
-  if(current == NULL){
-    myRootingTable = r; 
-  }
-  else{
-    while(current->next!=NULL){
-      current = current->next; 
-    }
-    current->next = r;
-  }
-    
-}
-void freeRoutingTable(){
-  struct rootingTable* current = myRootingTable;
-  if(current == NULL){
-    return;
-  }
-  while(current->next != NULL){
-    struct rootingTable *old = current;
-    current = current->next;
-    free(old); 
-  }
-
-}
-static void
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
-{
-  struct broadcastMessage * b = packetbuf_dataptr();
-  if(b->reset){
-    treeBuilded = false;
-  }
-  printf("broadcast message received from %d.%d: '%d'\n",
-         from->u8[0], from->u8[1], b->hop);
-  if(b->reset){
-      b->hop++;
-      packetbuf_copyfrom(b, sizeof(struct broadcastMessage));
-      broadcast_send(&broadcast);
-      printf("reset send \n");
-    }
-  
-}
-
-static void
-recv_uc(struct unicast_conn *c, const linkaddr_t *from)
-{
-  struct unicastMessage * u = packetbuf_dataptr();
-  //printf("%d\n", u->from_addr.u8[0]);
-  if(u->reg){
-    printf("received reg message from %d.%d\n", from->u8[0],from->u8[1]);
-    addToRootingTable(u->from_addr);
-    
-  }
-}
-
-static void 
-sent_uc(struct unicast_conn *c, int status, int num_tx)
-{
-  const linkaddr_t *dest = packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
-  if(linkaddr_cmp(dest, &linkaddr_null)) {
-    return;
-  }
-  printf("unicast message sent to %d.%d: status %d num_tx %d\n",
-    dest->u8[0], dest->u8[1], status, num_tx);
-}
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
-
-void myExitHandler(){
-  unicast_close(&uc);
-  broadcast_close(&broadcast);
-}
-static const struct unicast_callbacks unicast_callbacks = {recv_uc, sent_uc};
-
-PROCESS_THREAD(root, ev, data)
-{
-  static struct etimer et;
-
-  PROCESS_EXITHANDLER(myExitHandler();)
-
-  PROCESS_BEGIN();
-
-  broadcast_open(&broadcast, 129, &broadcast_call);
-  unicast_open(&uc, 146, &unicast_callbacks);
-  while(1) {
-
-    // Delay 2-4 seconds 
-    etimer_set(&et, CLOCK_SECOND * 15+ random_rand() % (CLOCK_SECOND * 4 ));
-
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-    if(!treeBuilded){
-      struct broadcastMessage b;
-      b.hop = 0;
-      b.treebuilder = true;
-      b.reset = false;
-      packetbuf_copyfrom(&b, sizeof(struct broadcastMessage));
-      broadcast_send(&broadcast);
-      treeBuilded = true;
-      printf("broadcast message sent\n");
-    }
-  }
-  
-  PROCESS_END();
-}
-*/
-/*---------------------------------------------------------------------------*/
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
 
 #include "contiki.h"
 #include "random.h"
@@ -218,7 +39,13 @@ struct rootingTable{
   linkaddr_t addr; 
   linkaddr_t nextHop;
   unsigned long lastCom; 
-
+  bool metered;
+  struct data *head;
+  int dataCount;
+};
+struct data{
+  int value;
+  struct data * next;
 };
 linkaddr_t nullAddr; 
 
@@ -235,7 +62,7 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
 
   struct broadcastMessage * b = packetbuf_dataptr();
-  printf("broadcast message received from %d.%d: '%d'\n",
+
          from->u8[0], from->u8[1], b->hop);
   if(b->reset){
     resetNetwork();
@@ -254,7 +81,7 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
     packetbuf_copyfrom(b, sizeof(struct broadcastMessage));
     broadcast_send(&broadcast);
   
-    printf("broadcast message sent\n");
+
   }
 }
 static void
@@ -262,7 +89,7 @@ recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
 
   struct unicastMessage * u = packetbuf_dataptr();
-  printf("root received a message from %d via %d\n", u->from_addr.u8[0], from->u8[0]);
+
   struct rootingTable * current= myRootingTable;
   while(current!=NULL){
     if(current->addr.u8[0] == from->u8[0] && current->addr.u8[1] == from->u8[1]){
@@ -271,25 +98,25 @@ recv_uc(struct unicast_conn *c, const linkaddr_t *from)
     }
     current=current->next;
   }
-  printf("timer updated \n");
+
   if(u->reg){
     
-    printf("received reg message from %d.%d\n", from->u8[0],from->u8[1]);
+
   
     addToRootingTable(u->from_addr, *from);
     leds_off(LEDS_ALL);
   }
   if(u->dest_addr.u8[0] == from->u8[0] && u-> dest_addr.u8[1] == from->u8[1]){
-    printf("ack received\n");
+
     return; 
   }
-  printf("ack sening...\n");
+
   if(linkaddr_cmp(&nullAddr, &(u->dest_addr))){
     
-    struct unicastMessage u2;
+      struct unicastMessage u2;
       u2.data = true; 
       u2.reg = false;
-      u2.temperature = random_rand();
+      u2.temperature = u->temperature;
       u2.from_addr = linkaddr_node_addr;
       u2.dest_addr.u8[0] = u->from_addr.u8[0];
       u2.dest_addr.u8[1] = u->from_addr.u8[1]; 
@@ -301,14 +128,51 @@ recv_uc(struct unicast_conn *c, const linkaddr_t *from)
         current=current->next;
       }
       if(current==NULL){
-        printf("ROOTING TABLE ERROR, head:%d, trying to compare %d and %d\n", myRootingTable->addr.u8[0], u2.dest_addr.u8[0], current->addr.u8[0]);
+
         resetNetwork();
       }
       else{
-        printf("ack send to %d\n", current->addr.u8[0]);
+
         unicast_send(&uc, &(current->nextHop));
         
       }
+      struct data * new = malloc(sizeof(struct data));
+      new->value=u->temperature;
+      new->next=current->head;
+      current->head = new;
+      current->dataCount++;
+
+    if(current->dataCount == 10 ){
+      
+      double y[10];
+      double x[10];
+      int i = 1;
+      while(i<11){
+        y[i]=current->head->value;
+        x[i]=i;
+        struct data *t = current->head;
+        current->head = current->head->next;
+        free(t);
+        i++;
+      }
+      current->dataCount=0;
+      double a;
+      double b;
+      //linreg(10,x,y,&a,&b,NULL);
+      
+      if(a>0.0){
+
+        struct unicastMessage u3;
+        u3.data = true; 
+        u3.reg = false;
+        u3.temperature = -1;
+        u3.from_addr = linkaddr_node_addr;
+        u3.dest_addr = current->addr;
+        
+        packetbuf_copyfrom(&u3, sizeof(struct unicastMessage));
+        unicast_send(&uc, &(current->nextHop));
+      }
+    }
       
     //processing
   }
@@ -332,7 +196,7 @@ sent_uc(struct unicast_conn *c, int status, int num_tx)
   if(linkaddr_cmp(dest, &linkaddr_null)) {
     return;
   }
-  printf("unicast message sent to %d.%d: status %d num_tx %d\n",
+
     dest->u8[0], dest->u8[1], status, num_tx);
 }
 void addToRootingTable(linkaddr_t addr, linkaddr_t nexthop){
@@ -376,7 +240,7 @@ void resetNetwork(){
   b.hop = 0;
   packetbuf_copyfrom(&b, sizeof(struct broadcastMessage));
   broadcast_send(&broadcast);
-  printf("RESET ENGAGE\n");
+
   freeRoutingTable();
   myRootingTable=NULL;
 }
@@ -408,7 +272,7 @@ PROCESS_THREAD(root, ev, data)
       packetbuf_copyfrom(&b, sizeof(struct broadcastMessage));
       broadcast_send(&broadcast);
       treeBuilded = true;
-      printf("broadcast message sent\n");
+
     }
   }
   
